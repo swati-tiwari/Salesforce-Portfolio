323cd8900c80d21b112661a9e06aa2d39e4e6b08
Swati Tiwari <63907055+swati-tiwari@users.noreply.github.com>
2024-06-22 16:01:54 +0000

    Updated js

--- js/main.js
+++ js/main.js
@@ -129,17 +129,17 @@ var span = document.getElementsByClassName("oldclose")[0];
 
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
-  oldmodal.style.display = "block";
+  modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-  oldmodal.style.display = "none";
+  modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == oldmodal) {
-    oldmodal.style.display = "none";
+  if (event.target == modal) {
+    modal.style.display = "none";
   }
 }
