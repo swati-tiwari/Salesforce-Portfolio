1a1095172cb85016175742f42bb164ecb041679c
Swati Tiwari <63907055+swati-tiwari@users.noreply.github.com>
2024-06-22 15:46:00 +0000

    Added modal on portfolio

--- js/main.js
+++ js/main.js
@@ -115,3 +115,31 @@ $(document).ready(function () {
     v = 300;
   f.addEventListener("input", c);
 });
+
+/******* OLD Portfolio Modal *******/ 
+
+// Get the modal
+var modal = document.getElementById("oldPortfolioModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("oldclose")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  oldmodal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  oldmodal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == oldmodal) {
+    oldmodal.style.display = "none";
+  }
+}
